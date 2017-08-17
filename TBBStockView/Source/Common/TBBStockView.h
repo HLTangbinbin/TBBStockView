1abb8917157a8ff4f55b5440598f460b5d4aff02
HLTangbinbin <m18513848880@163.com>
2017-08-17 23:14:58 +0800

    bbbb

--- TBBStockView/Source/Common/TBBStockView.h
+++ TBBStockView/Source/Common/TBBStockView.h
@@ -1,10 +1,10 @@
 //
 //  TBBStockView.h
-//  TBBLineChartView
 //
 //  Created by 唐彬彬 on 2017/7/9.
 //  Copyright © 2017年 唐彬彬. All rights reserved.
 //
+
 #import "Common.h"
 #import "UIColor+HL.h"
 #import "TBBTimeLineView.h"
